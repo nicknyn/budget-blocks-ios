b463b62bd7725129435bf2c2ac014441f827a0e3
Isvvc <lyons@tuta.io>
2020-01-27 10:30:35 -0700

    Create NetworkingController class with empty function definitions

---
+++ Budget Blocks/Budget Blocks/Networking/NetworkingController.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkingController.swift
+//  Budget Blocks
+//
+//  Created by Isaac Lyons on 1/27/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import Foundation
+
+class NetworkingController {
+    private let baseURL = URL(string: "https://lambda-budget-blocks.herokuapp.com/api/")!
+    
+    func login(email: String, password: String) {
+        
+    }
+    
+    func register(email: String, password: String) {
+        
+    }
+}
