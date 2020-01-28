797890399e4318491bff7c9d4627d9870ffa6ca4
Isvvc <lyons@tuta.io>
2020-01-27 17:47:46 -0700

    Implement login function

--- Budget Blocks/Budget Blocks/Networking/NetworkingController.swift
+++ Budget Blocks/Budget Blocks/Networking/NetworkingController.swift
@@ -7,12 +7,49 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
 
 class NetworkingController {
     private let baseURL = URL(string: "https://lambda-budget-blocks.herokuapp.com/api/")!
     
-    func login(email: String, password: String) {
+    func login(email: String, password: String, completion: @escaping (String?, Error?) -> Void) {
+        let loginJSON = JSON(dictionaryLiteral: ("email", email), ("password", password))
         
+        let url = baseURL.appendingPathComponent("auth")
+            .appendingPathComponent("login")
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            request.httpBody = try loginJSON.rawData()
+            URLSession.shared.dataTask(with: request) { data, _, error in
+                if let error = error {
+                    return completion(nil, error)
+                }
+                
+                guard let data = data else {
+                    NSLog("No data returned from login request")
+                    return completion(nil, nil)
+                }
+                
+                do {
+                    let responseJSON = try JSON(data: data)
+                    completion(responseJSON["token"].string, nil)
+                } catch {
+                    completion(nil, error)
+                }
+            }.resume()
+        } catch {
+            completion(nil, error)
+        }
     }
     
     func register(email: String, password: String) {
