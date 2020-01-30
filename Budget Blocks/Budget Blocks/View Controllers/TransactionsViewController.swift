c0e227b7814c2fc277f8e2ee4435cbeb01921c66
Isvvc <lyons@tuta.io>
2020-01-30 16:59:01 -0700

    List transactions on transaction view controller

--- Budget Blocks/Budget Blocks/View Controllers/TransactionsViewController.swift
+++ Budget Blocks/Budget Blocks/View Controllers/TransactionsViewController.swift
@@ -11,8 +11,35 @@ import CoreData
 
 class TransactionsViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var networkingController: NetworkingController!
     let transactionController = TransactionController()
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Transaction> = {
+        let fetchRequest: NSFetchRequest<Transaction> = Transaction.fetchRequest()
+        
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(key: "date", ascending: false),
+            NSSortDescriptor(key: "transactionID", ascending: true)
+        ]
+        
+        let context = CoreDataStack.shared.mainContext
+        
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: context,
+                                             sectionNameKeyPath: nil,
+                                             cacheName: nil)
+        frc.delegate = self
+        
+        do {
+            try frc.performFetch()
+        } catch {
+            fatalError("Error fetching transactions: \(error)")
+        }
+        
+        return frc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +49,72 @@ class TransactionsViewController: UIViewController {
             if let error = error {
                 return NSLog("\(error)")
             }
-            
-            let fetchRequest: NSFetchRequest<Transaction> = Transaction.fetchRequest()
-            let transactions = try! CoreDataStack.shared.mainContext.fetch(fetchRequest)
-            print(transactions.map({ $0.amount }))
         }
     }
 
+}
+
+// MARK: Table view data source and delegate
+
+extension TransactionsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.fetchedObjects?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionCell", for: indexPath)
+        
+        let transaction = fetchedResultsController.object(at: indexPath)
+        cell.textLabel?.text = transaction.name
+        cell.detailTextLabel?.text = "$\(transaction.amount)"
+        
+        return cell
+    }
+}
+
+// MARK: Fetched results controller delegate
+
+extension TransactionsViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        let indexSet = IndexSet(integer: sectionIndex)
+        
+        switch type {
+        case .insert:
+            tableView.insertSections(indexSet, with: .automatic)
+        case .delete:
+            tableView.deleteSections(indexSet, with: .automatic)
+        default:
+            return
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let indexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.moveRow(at: indexPath, to: newIndexPath)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            fatalError()
+        }
+    }
+    
 }
