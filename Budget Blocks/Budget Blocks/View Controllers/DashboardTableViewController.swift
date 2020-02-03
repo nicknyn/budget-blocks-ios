e34c18d38d8ba9763c78e155e58602b21f1381c8
Isvvc <lyons@tuta.io>
2020-02-03 16:39:32 -0700

    Create a basic dashboard table view controller

---
+++ Budget Blocks/Budget Blocks/View Controllers/DashboardTableViewController.swift
@@ -0,0 +1,164 @@
+//
+//  DashboardTableViewController.swift
+//  Budget Blocks
+//
+//  Created by Isaac Lyons on 2/3/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import UIKit
+import LinkKit
+import CoreData
+
+class DashboardTableViewController: UITableViewController {
+    
+    let networkingController = NetworkingController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if networkingController.bearer == nil {
+            performSegue(withIdentifier: "InitialLogin", sender: self)
+        }
+        
+        // Temporary logout button until the profile page is set up
+        let logoutButton = UIBarButtonItem(title: "Sign out", style: .plain, target: self, action: #selector(logout))
+        navigationItem.rightBarButtonItem = logoutButton
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DashboardCell", for: indexPath)
+
+        let cellText: String
+        if indexPath.section == 0 {
+            cellText = "Connect your bank with Plaid"
+        } else {
+            cellText = "Transactions"
+        }
+        cell.textLabel?.text = cellText
+        
+        if let cellTextSize = cell.textLabel?.font.pointSize {
+            cell.textLabel?.font = UIFont(name: "Exo-Regular", size: cellTextSize)
+        }
+        
+        cell.backgroundColor = UIColor(red:0.93, green:0.93, blue:0.93, alpha:1.0)
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 4
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            linkAccount()
+        } else {
+            self.performSegue(withIdentifier: "ShowTransactions", sender: self)
+        }
+    }
+    
+    // MARK: Private
+    
+    private func linkAccount() {
+        guard let publicKey = ProcessInfo.processInfo.environment["PLAID_PUBLIC_KEY"] else {
+            return NSLog("No public key found!")
+        }
+        let linkConfiguration = PLKConfiguration(key: publicKey, env: .sandbox, product: .auth)
+        let linkViewController = PLKPlaidLinkViewController(configuration: linkConfiguration, delegate: self)
+        present(linkViewController, animated: true)
+    }
+    
+    @objc private func logout() {
+        networkingController.logout()
+        TransactionController().clearStoredTransactions(context: CoreDataStack.shared.mainContext)
+        performSegue(withIdentifier: "AnimatedLogin", sender: self)
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let welcomeVC = segue.destination as? WelcomeViewController {
+            welcomeVC.networkingController = networkingController
+        } else if let transactionsVC = segue.destination as? TransactionsViewController {
+            transactionsVC.networkingController = networkingController
+        }
+    }
+
+}
+
+// MARK: Plaid Link view delegate
+
+extension DashboardTableViewController: PLKPlaidLinkViewDelegate {
+    func linkViewController(_ linkViewController: PLKPlaidLinkViewController, didSucceedWithPublicToken publicToken: String, metadata: [String : Any]?) {
+        print("Link successful. Public token: \(publicToken)")
+        networkingController.tokenExchange(publicToken: publicToken) { error in
+            if let error = error {
+                NSLog("Error exchanging token: \(error)")
+            }
+        }
+        dismiss(animated: true)
+    }
+    
+    func linkViewController(_ linkViewController: PLKPlaidLinkViewController, didExitWithError error: Error?, metadata: [String : Any]?) {
+        if let error = error {
+            NSLog("Error linking bank account: \(error)")
+        } else {
+            NSLog("Error linking bank account.")
+        }
+    }
+}
