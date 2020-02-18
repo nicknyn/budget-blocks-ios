d06eaa585032ed2a560f684fcb7493f83bb8dfa0
Isvvc <lyons@tuta.io>
2020-02-18 13:53:28 -0700

    Create categories table view for creating a transactions

---
+++ Budget Blocks/Budget Blocks/View Controllers/CategoriesTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  CategoriesTableViewController.swift
+//  Budget Blocks
+//
+//  Created by Isaac Lyons on 2/18/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoriesTableViewController: UITableViewController {
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<TransactionCategory> = {
+        let fetchRequest: NSFetchRequest<TransactionCategory> = TransactionCategory.fetchRequest()
+        
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(key: "name", ascending: true)
+        ]
+        
+        let context = CoreDataStack.shared.mainContext
+        
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: context,
+                                             sectionNameKeyPath: nil,
+                                             cacheName: nil)
+        
+        do {
+            try frc.performFetch()
+        } catch {
+            fatalError("Error fetching transactions: \(error)")
+        }
+        
+        return frc
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.fetchedObjects?.count ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+
+        let category = fetchedResultsController.object(at: indexPath)
+        cell.textLabel?.text = category.name
+        if let transactionCount = category.transactions?.count, transactionCount > 0 {
+            cell.detailTextLabel?.text = "\(transactionCount) recent transactions"
+        } else {
+            cell.detailTextLabel?.text = nil
+        }
+
+        return cell
+    }
+
+}
