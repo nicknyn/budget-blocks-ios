c1b72eaed1b5f693cfa8ee20de3f3762a42b0707
Nick <ptnguyen1901@gmail.com>
2020-06-01 15:57:05 -0400

    Add Transaction model.

---
+++ Budget Blocks/Budget Blocks/Model/OnlineTransaction.swift
@@ -0,0 +1,102 @@
+//
+//  OnlineTransaction.swift
+//  Budget Blocks
+//
+//  Created by Nick Nguyen on 6/1/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import Foundation
+
+struct OnlineTransactions: Codable {
+    let transactions : [OnlineTransaction]
+}
+
+struct OnlineTransaction: Codable {
+    let accountId : String? // account_id
+    let accountOwner: String? // account_owner
+    let amount : Double?
+    let authorizedDate: Date? // authorized_date
+    let category : [String]
+    let categoryId : Int?
+    let date : Date?
+    let isoCurrencyCode : String? // iso_currency_code
+    let location : Location
+    let name: String?
+    let paymentChannel: String? // payment_channel
+    let paymentMeta : PaymentMeta
+    let pending: Bool?
+    let pendingTransactionId: Int?
+    let transactionCode: Int?
+    let transactionId: String?
+    let transactionType: String?
+    let unofficialCurrencyCode: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case accountId              = "account_id"
+        case accountOwner           = "account_owner"
+        case amount                 = "amount"
+        case authorizedDate         = "authorized_date"
+        case category               = "category"
+        case categoryId             = "category_id"
+        case date                   = "date"
+        case isoCurrencyCode        = "iso_currency_code"
+        case location               = "location"
+        case name                   = "name"
+        case paymentChannel         = "payment_channel"
+        case paymentMeta            = "payment_meta"
+        case pending                = "pending"
+        case pendingTransactionId   = "pending_transaction_id"
+        case transactionCode        = "transaction_code"
+        case transactionId          = "transaction_id"
+        case transactionType        = "transaction_type"
+        case unofficialCurrencyCode = "unofficial_currency_code"
+        
+    }
+    
+}
+
+struct Location: Codable {
+    let address: String?
+    let city: String?
+    let country: String?
+    let latitude: Double?
+    let longitude: Double?
+    let postalCode: Double? // postal_code
+    let region: String?
+    let storeNumber: Int? // store_number
+    
+    enum CodingKeys: String,CodingKey {
+        case address = "address"
+        case city = "city"
+        case country = "country"
+        case latitude = "lat"
+        case longitude = "lon"
+        case postalCode = "postal_code"
+        case region = "region"
+        case storeNumber = "store_number"
+    }
+}
+
+
+struct PaymentMeta: Codable {
+    let byOrderOf: String?
+    let payee: String?
+    let payer: String?
+    let paymentMethod: String?
+    let paymentProcessor: String?
+    let ppdID: Int?
+    let reason: String?
+    let referenceNumber: Int?
+    
+    enum CodingKeys: String,CodingKey {
+        case byOrderOf = "by_order_of"
+        case payee = "payee"
+        case payer = "payer"
+        case paymentMethod = "payment_method"
+        case paymentProcessor = "payment_processor"
+        case ppdID = "ppd_id"
+        case reason = "reason"
+        case referenceNumber = "reference_number"
+    }
+}
